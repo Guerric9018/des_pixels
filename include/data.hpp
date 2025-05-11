6a42aeb028d8fa36a9ff8491969441e50471c720
lune <lune.taken@gmail.com>
2025-05-11 12:49:50 +0200

    change clustering algorithm to support discontiguous patches of color

--- include/data.hpp
+++ include/data.hpp
@@ -11,20 +11,6 @@ struct Clusters
 {
 	using cluster = std::vector<Vertex>;
 	std::vector<cluster> data;
-
-	void add(byte *colors, int id)
-	{
-		for (auto &c : data) {
-			auto *color = &colors[c[0].id * 3];
-			if (color[0] == colors[id * 3 + 0]
-			 && color[1] == colors[id * 3 + 1]
-			 && color[2] == colors[id * 3 + 2]) {
-				c.push_back(Vertex{ id });
-				return;
-			}
-		}
-		data.push_back(std::vector{ Vertex{ id } });
-	}
 };
 
 struct vec2
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,80 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <map>
 #include "data.hpp"
+
+struct union_find
+{
+	std::vector<unsigned> data;
+
+	union_find(size_t sz) : data(sz)
+	{
+		for (size_t i = 0; i < sz; ++i) {
+			data[i] = i;
+		}
+	}
+
+	unsigned find(unsigned id)
+	{
+		unsigned at = id;
+		while (at != data[at]) {
+			at = data[at];
+		}
+		return data[id] = at;
+	}
+
+	unsigned unite(unsigned a, unsigned b)
+	{
+		return data[find(b)] = find(a);
+	}
+};
+
+bool same_color(byte *data, size_t a, size_t b)
+{
+	auto d0 = data[a * 3 + 0] - data[b * 3 + 0];
+	auto d1 = data[a * 3 + 1] - data[b * 3 + 1];
+	auto d2 = data[a * 3 + 2] - data[b * 3 + 2];
+	return d0*d0 + d1*d1 + d2*d2 <= 8*8;
+}
 
 Clusters find_clusters(byte *data, size_t width, size_t height)
 {
 	Clusters c;
-	for (size_t x = 0; x < width; ++x) {
-		for (size_t y = 0; y < height; ++y) {
-			auto id = y * height + x;
-			c.add(data, id);
+
+	auto index = [=] (size_t x, size_t y) { return x + y * width; };
+	size_t offsets[] = {
+		+1,
+		-1 + width,
+		+width,
+		+1 + width,
+	};
+	union_find uf(width * height);
+	for (size_t y = 0; y < height; ++y) {
+		for (size_t x = 0; x < width; ++x) {
+			auto base = index(x, y);
+			for (auto offset : offsets) {
+				auto at = base + offset;
+				if (at >= uf.data.size()) {
+					continue;
+				}
+				if (same_color(data, base, at)) {
+					uf.unite(base, at);
+				}
+			}
 		}
 	}
+
+	std::map<unsigned, std::vector<size_t>> mapping;
+	for (size_t y = 0; y < height; ++y) {
+		for (size_t x = 0; x < width; ++x) {
+			auto i = index(x, y);
+			mapping[uf.find(i)].push_back(i);
+		}
+	}
+
+	c.data = std::vector<Clusters::cluster>(mapping.size());
+
 	return c;
 }
 
@@ -24,6 +87,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	int width, height, channels;
+	stbi_set_flip_vertically_on_load(false);
 	auto pixels = stbi_load(argv[1], &width, &height, &channels, 0);
 	assert(channels == 3);
 	assert(width > 0 && height > 0);
